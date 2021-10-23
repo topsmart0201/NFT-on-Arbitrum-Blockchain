18c7c1aa1e780fca9e4b306655c3fbe145501d74
topsmart20 <oswellgrundmeier42@gmail.com>
2021-10-24 03:08:55 +0900

    Updated  contract address and social link

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const override = css`
   border: 3px dashed;
 `;
 
-const CONTRACT_ADDRESS = "0x8B058fd75d64fB0Fec8395A4ad50f28291BfEbe3";
+const CONTRACT_ADDRESS = "0x96fc6c5261E4AF65f93f45277f004e26b7cb42a1";
 const METADATA_BASE_TOKENURI = "https://gateway.pinata.cloud/ipfs/QmWqiQCrcA2zGasYXPjC3XJVChNeUGdapC1k61qmdk146d/";
 
 const App = () => {
@@ -323,15 +323,15 @@ const App = () => {
               <footer className="footer">
                   <h1 style={{marginTop: "10px"}}>Crypto Ricks © 2021</h1>
                   <p>Smart Contract Address: <a
-                          href="https://arbiscan.io/address/0x53930807383be7139e1da1a758370cd64469ee43">0x8B058fd75d64fB0Fec8395A4ad50f28291BfEbe3</a>
+                          href="https://arbiscan.io/address/0x53930807383be7139e1da1a758370cd64469ee43">0x96fc6c5261E4AF65f93f45277f004e26b7cb42a1</a>
                   </p>
-                  <div className="socials"><a href="https://twitter.com/YAYBEAR"><span><svg stroke="currentColor"
+                  <div className="socials"><a href="https://twitter.com/CryptoRicksNFT"><span><svg stroke="currentColor"
                                   fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em"
                                   xmlns="http://www.w3.org/2000/svg">
                                   <path
                                       d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z">
                                   </path>
-                              </svg>      </span></a><a href="https://discord.gg/p9aYnfBBab"><span><svg
+                              </svg>      </span></a><a href="https://discord.com/invite/Uzef6EeD5Y"><span><svg
                                   stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512"
                                   height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                   <path
